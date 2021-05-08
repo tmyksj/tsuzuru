6e63f594a5cb900e8e3ec5eb5672a0c1c691b92d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-05-08 09:19:18 +0900

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("org.springframework.boot") version "2.4.1"
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.jpa") version "1.4.21"
-    kotlin("plugin.spring") version "1.4.21"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.4.5"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.jpa") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
 }
 
 group = "tsuzuru"
